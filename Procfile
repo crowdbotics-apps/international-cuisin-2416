06246df1795b186effb6d88c1e33276c143d8f1b
Crowdbotics <team@crowdbotics.com>
2019-04-18 19:30:24 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.international_cuisin_2416.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/international_cuisin_2416/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for international_cuisin_2416 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "international_cuisin_2416.settings")
+
+application = get_wsgi_application()
